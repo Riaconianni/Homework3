5d2239eb5524f96f2edb8798278e4855cc22d804
Rachel Iaconianni <iaconiannir@gmail.com>
2019-12-09 13:32:40 -0500

    created variables for numbers, letters, special characters, blank array, and if else statement for password length

--- script.js
+++ script.js
@@ -12,7 +12,22 @@ function writePassword() {
   copyBtn.removeAttribute("disabled");
   copyBtn.focus();
 }
+//Vars for characters used to make up generated password
+var numbers="0123456789";
+var lowerLetters="abcdefghijklmnopqrstuvwxyz";
+var upperLetters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChar="!@#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+var newPassword=[];
 // Create Prompt for user to choose 8-128 characters for their password
+var passwordLength=prompt("How many characters would you like in your password? Please choose a number from 8-128");
+
+if (passwordLength >= 8 && passwordLength <= 128) {
+  alert("Thank you");
+}
+else {
+  alert("Tsk Tsk! Try again!")
+}
+console.log(passwordLength);
 // Create Prompt for user to choose if they would like special characters
 // Create Prompt for user to choose if they would like lowercase letters
 // Create Prompt for user to choose if they would like uppercase letters
