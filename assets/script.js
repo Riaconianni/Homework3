38b05c8af589c0a0ae19e9af82dc2bb7b6f9ef88
Rachel Iaconianni <iaconiannir@gmail.com>
2019-12-10 14:05:12 -0500

    editted wording of alerts, added userNum

--- assets/script.js
+++ assets/script.js
@@ -14,10 +14,11 @@ function writePassword() {
 }
 //Vars for characters used to make up generated password
 var numbers="0123456789";
-var lowerLetters="abcdefghijklmnopqrstuvwxyz";
-var upperLetters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var specialChar="!@#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+var lowerCase="abcdefghijklmnopqrstuvwxyz";
+var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var special="!@#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 var newPassword=[];
+
 // Create Prompt for user to choose 8-128 characters for their password
 var passwordLength=prompt("How many characters would you like in your password? Please choose a number from 8-128");
 
@@ -26,18 +27,28 @@ if (passwordLength >= 8 && passwordLength <= 128) {
 }
 else {
   alert("Tsk Tsk! Try again!");
-}
+} //need to make loop so person has to type correct amount
 console.log(passwordLength);
 
+//Create confirm for user to choose if they would like numbers
+var userNum=confirm("Would you like numbers in your password?");
+
+if (userNum === true) {
+  alert("Your password will contain numbers.");
+}
+else {
+  alert("Your password will not contain numbers.");
+}
+console.log(userNum);
 // Create Prompt for user to choose if they would like special characters
 
 var userSpecialChar=confirm("Would you like special characters?");
 
 if (userSpecialChar === true) {
-  alert("Adding Special Characters to your password.");
+  alert("Your password will contain special characters.");
 }
 else {
-  alert("No special characters will be added to your password");
+  alert("Your password will not contain special characters.");
 }
 console.log(userSpecialChar);
 
@@ -46,25 +57,28 @@ console.log(userSpecialChar);
 var userLowerCase=confirm("Would you like lowercase letters in your password?");
 
 if (userLowerCase === true) {
-  alert("Your password will have lowercase letters.");
+  alert("Your password will contain lowercase letters.");
 }
 else {
-  alert("Your password will have no lowercase letters.");
+  alert("Your password will not contain lowercase letters.");
 }
 console.log(userLowerCase);
+
 // Create confirm for user to choose if they would like uppercase letters
 
 var userUpperCase=confirm("Would you like uppercase letters in your password?");
 
 if (userUpperCase === true) {
-  alert("Your password will have uppercase letters.");
+  alert("Your password will contain uppercase letters.");
 }
 else {
-  alert("Your password will have no uppercase letters.");
-}
+  alert("Your password will not contain uppercase letters.");
+};
 console.log(userUpperCase);
-// Validate at least one of prompts is true
 
+// Validate at least one of prompts is true
+//Create while loop that adds each true variable to the empty array called newPassword
+//Use the newPassword array to generate the password
 function copyToClipboard() {
   // BONUS 
 }
